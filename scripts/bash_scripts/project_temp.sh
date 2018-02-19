838bd1af73a42d182804bd116a3599f0f511e42f
ramakant_mishra <mishrark16@gmail.com>
2018-02-19 17:31:47 +0100

    project template creator

---
+++ scripts/bash_scripts/project_temp.sh
@@ -0,0 +1,20 @@
+mkdir $1
+cd $1
+mkdir doc
+cd doc
+touch "readme.txt"
+cd ..
+mkdir data
+cd data
+mkdir raw_dataset
+mkdir feature_set
+mkdir cv_set
+cd ..
+mkdir scripts
+cd scripts
+mkdir bash_scripts
+cd ..
+mkdir models
+
+
+
